48d9b31dfed9bbe21ba2465ae99a4c54983f1003
mahdi_mef <mahdieshghi201616@gmail.com>
2019-01-23 20:32:42 +0330

    all file OK 18 jan 2018

--- AddFormoulForm.php
+++ AddFormoulForm.php
@@ -1,7 +1,27 @@
 <html>
-<title>Add formoul file </title>
+<title>Add formoul form </title>
+<head>
+    <script>
+        function validateForm() {
+            var T = document.forms["myForm"]["title"].value;
+            var C = document.forms["myForm"]["content"].value;
+            var I = document.forms["myForm"]["image"].value;
+            var CL = document.forms["myForm"]["class"].value;
+
+
+            if (T != "" && C != "" && I != "" && CL != "") {
+                return true;
+            } else {
+                alert("##### فیلد ها نباید خالی باشد ####");
+                return false
+            }
+        }
+    </script>
+</head>
 <body>
-<form action="AddformoulResult.php" method="post">
+
+<form name="myForm" action="AddformoulResult.php" method="post" onsubmit="return validateForm()">
+    <br>
     formoul title : <br>
     <input type="text" name="title"><br>
     formoul content : <br>
@@ -12,7 +32,39 @@
     class: <br>
     <input type="text" name="class">
     <br>
-    <input type="submit" name="btn_submit" value="save formoul"></input>
+    <br>
+    <input type="submit" name="btn_submit" value="Submit"
+           class="button">
+
+    <style>
+        .button {
+            background-color: #4CAF50;
+            border: none;
+            color: white;
+            text-align: center;
+            font-size: 16px;
+            margin: 4px 2px;
+            height: 45px;
+            width: 150px;
+            border-radius: 25px;
+
+        }
+
+        input[type=text] {
+            width: 90%;
+            padding: 12px 20px;
+            margin: 8px 8px;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+
+        }
+
+        input[type=submit]:hover {
+            background-color: #49a000;
+        }
+
+
+    </style>
 
 </form>
 </body>
--- getFormoul.php
+++ getFormoul.php
@@ -5,7 +5,6 @@
  * Date: 1/18/2019
  * Time: 1:25 PM
  */
-echo "hiiiii";
 include 'DatabaseManager.php';
 $databsemanager = new DatabaseManager();
 $databsemanager->getFormoul();
